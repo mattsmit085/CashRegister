23cd3e43e42f2edb73a3483fb5f4355c575179b6
Matt Smith <mattsmit085@outlook.com>
2021-02-10 16:34:38 -0500

    Add project files.

---
+++ CashRegister/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿
+namespace CashRegister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.smallButton = new System.Windows.Forms.Button();
+            this.medButton = new System.Windows.Forms.Button();
+            this.lrgButton = new System.Windows.Forms.Button();
+            this.toppingsLabel = new System.Windows.Forms.Label();
+            this.pepperoniButton = new System.Windows.Forms.Button();
+            this.extrachzButton = new System.Windows.Forms.Button();
+            this.pineappleButton = new System.Windows.Forms.Button();
+            this.mushroomButton = new System.Windows.Forms.Button();
+            this.orderLabel = new System.Windows.Forms.Label();
+            this.ordertitleLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.neworderButton = new System.Windows.Forms.Button();
+            this.receiptLabel = new System.Windows.Forms.Label();
+            this.onionLabel = new System.Windows.Forms.Button();
+            this.greenpepperButton = new System.Windows.Forms.Button();
+            this.extrasLabel = new System.Windows.Forms.Label();
+            this.wingsButton = new System.Windows.Forms.Button();
+            this.garlicbreadButton = new System.Windows.Forms.Button();
+            this.saladButton = new System.Windows.Forms.Button();
+            this.receipttitleLabel = new System.Windows.Forms.Label();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pricingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkRed;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MistyRose;
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(529, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "  Pinos Pizza";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.sizeLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sizeLabel.Location = new System.Drawing.Point(134, 51);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(47, 22);
+            this.sizeLabel.TabIndex = 1;
+            this.sizeLabel.Text = "Size:";
+            // 
+            // smallButton
+            // 
+            this.smallButton.Location = new System.Drawing.Point(138, 76);
+            this.smallButton.Name = "smallButton";
+            this.smallButton.Size = new System.Drawing.Size(43, 23);
+            this.smallButton.TabIndex = 2;
+            this.smallButton.Text = "Sml";
+            this.smallButton.UseVisualStyleBackColor = true;
+            this.smallButton.Click += new System.EventHandler(this.smallButton_Click);
+            // 
+            // medButton
+            // 
+            this.medButton.Location = new System.Drawing.Point(187, 76);
+            this.medButton.Name = "medButton";
+            this.medButton.Size = new System.Drawing.Size(43, 23);
+            this.medButton.TabIndex = 3;
+            this.medButton.Text = "Med";
+            this.medButton.UseVisualStyleBackColor = true;
+            this.medButton.Click += new System.EventHandler(this.medButton_Click);
+            // 
+            // lrgButton
+            // 
+            this.lrgButton.Location = new System.Drawing.Point(236, 76);
+            this.lrgButton.Name = "lrgButton";
+            this.lrgButton.Size = new System.Drawing.Size(43, 23);
+            this.lrgButton.TabIndex = 4;
+            this.lrgButton.Text = "Lrg";
+            this.lrgButton.UseVisualStyleBackColor = true;
+            this.lrgButton.Click += new System.EventHandler(this.lrgButton_Click);
+            // 
+            // toppingsLabel
+            // 
+            this.toppingsLabel.AutoSize = true;
+            this.toppingsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.toppingsLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toppingsLabel.Location = new System.Drawing.Point(134, 109);
+            this.toppingsLabel.Name = "toppingsLabel";
+            this.toppingsLabel.Size = new System.Drawing.Size(92, 22);
+            this.toppingsLabel.TabIndex = 8;
+            this.toppingsLabel.Text = "Toppings:";
+            // 
+            // pepperoniButton
+            // 
+            this.pepperoniButton.Location = new System.Drawing.Point(138, 139);
+            this.pepperoniButton.Name = "pepperoniButton";
+            this.pepperoniButton.Size = new System.Drawing.Size(64, 43);
+            this.pepperoniButton.TabIndex = 9;
+            this.pepperoniButton.Text = "Pepperoni";
+            this.pepperoniButton.UseVisualStyleBackColor = true;
+            this.pepperoniButton.Click += new System.EventHandler(this.pepperoniButton_Click);
+            // 
+            // extrachzButton
+            // 
+            this.extrachzButton.Location = new System.Drawing.Point(208, 139);
+            this.extrachzButton.Name = "extrachzButton";
+            this.extrachzButton.Size = new System.Drawing.Size(64, 43);
+            this.extrachzButton.TabIndex = 10;
+            this.extrachzButton.Text = "Extra Cheese";
+            this.extrachzButton.UseVisualStyleBackColor = true;
+            this.extrachzButton.Click += new System.EventHandler(this.extrachzButton_Click);
+            // 
+            // pineappleButton
+            // 
+            this.pineappleButton.Location = new System.Drawing.Point(138, 188);
+            this.pineappleButton.Name = "pineappleButton";
+            this.pineappleButton.Size = new System.Drawing.Size(64, 43);
+            this.pineappleButton.TabIndex = 11;
+            this.pineappleButton.Text = "Pineapple";
+            this.pineappleButton.UseVisualStyleBackColor = true;
+            this.pineappleButton.Click += new System.EventHandler(this.pineappleButton_Click);
+            // 
+            // mushroomButton
+            // 
+            this.mushroomButton.Location = new System.Drawing.Point(208, 188);
+            this.mushroomButton.Name = "mushroomButton";
+            this.mushroomButton.Size = new System.Drawing.Size(64, 43);
+            this.mushroomButton.TabIndex = 12;
+            this.mushroomButton.Text = "Mushroom";
+            this.mushroomButton.UseVisualStyleBackColor = true;
+            this.mushroomButton.Click += new System.EventHandler(this.mushroomButton_Click);
+            // 
+            // orderLabel
+            // 
+            this.orderLabel.Location = new System.Drawing.Point(12, 81);
+            this.orderLabel.Name = "orderLabel";
+            this.orderLabel.Size = new System.Drawing.Size(121, 284);
+            this.orderLabel.TabIndex = 14;
+            // 
+            // ordertitleLabel
+            // 
+            this.ordertitleLabel.AutoSize = true;
+            this.ordertitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ordertitleLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ordertitleLabel.Location = new System.Drawing.Point(12, 51);
+            this.ordertitleLabel.Name = "ordertitleLabel";
+            this.ordertitleLabel.Size = new System.Drawing.Size(62, 22);
+            this.ordertitleLabel.TabIndex = 15;
+            this.ordertitleLabel.Text = "Order:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 29);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Calculate Order";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // neworderButton
+            // 
+            this.neworderButton.Location = new System.Drawing.Point(394, 368);
+            this.neworderButton.Name = "neworderButton";
+            this.neworderButton.Size = new System.Drawing.Size(117, 29);
+            this.neworderButton.TabIndex = 19;
+            this.neworderButton.Text = "New Order";
+            this.neworderButton.UseVisualStyleBackColor = true;
+            // 
+            // receiptLabel
+            // 
+            this.receiptLabel.Font = new System.Drawing.Font("MS UI Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptLabel.Location = new System.Drawing.Point(348, 95);
+            this.receiptLabel.Name = "receiptLabel";
+            this.receiptLabel.Size = new System.Drawing.Size(119, 270);
+            this.receiptLabel.TabIndex = 17;
+            // 
+            // onionLabel
+            // 
+            this.onionLabel.Location = new System.Drawing.Point(278, 188);
+            this.onionLabel.Name = "onionLabel";
+            this.onionLabel.Size = new System.Drawing.Size(64, 43);
+            this.onionLabel.TabIndex = 21;
+            this.onionLabel.Text = "Onions";
+            this.onionLabel.UseVisualStyleBackColor = true;
+            this.onionLabel.Click += new System.EventHandler(this.onionLabel_Click);
+            // 
+            // greenpepperButton
+            // 
+            this.greenpepperButton.Location = new System.Drawing.Point(278, 139);
+            this.greenpepperButton.Name = "greenpepperButton";
+            this.greenpepperButton.Size = new System.Drawing.Size(64, 43);
+            this.greenpepperButton.TabIndex = 20;
+            this.greenpepperButton.Text = "Green Peppers";
+            this.greenpepperButton.UseVisualStyleBackColor = true;
+            this.greenpepperButton.Click += new System.EventHandler(this.greenpepperButton_Click);
+            // 
+            // extrasLabel
+            // 
+            this.extrasLabel.AutoSize = true;
+            this.extrasLabel.BackColor = System.Drawing.Color.Transparent;
+            this.extrasLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.extrasLabel.Location = new System.Drawing.Point(138, 245);
+            this.extrasLabel.Name = "extrasLabel";
+            this.extrasLabel.Size = new System.Drawing.Size(64, 22);
+            this.extrasLabel.TabIndex = 22;
+            this.extrasLabel.Text = "Extras:";
+            // 
+            // wingsButton
+            // 
+            this.wingsButton.Location = new System.Drawing.Point(278, 274);
+            this.wingsButton.Name = "wingsButton";
+            this.wingsButton.Size = new System.Drawing.Size(64, 43);
+            this.wingsButton.TabIndex = 25;
+            this.wingsButton.Text = "Chicken Wings";
+            this.wingsButton.UseVisualStyleBackColor = true;
+            this.wingsButton.Click += new System.EventHandler(this.wingsButton_Click);
+            // 
+            // garlicbreadButton
+            // 
+            this.garlicbreadButton.Location = new System.Drawing.Point(208, 274);
+            this.garlicbreadButton.Name = "garlicbreadButton";
+            this.garlicbreadButton.Size = new System.Drawing.Size(64, 43);
+            this.garlicbreadButton.TabIndex = 24;
+            this.garlicbreadButton.Text = "Garlic Bread";
+            this.garlicbreadButton.UseVisualStyleBackColor = true;
+            this.garlicbreadButton.Click += new System.EventHandler(this.garlicbreadButton_Click);
+            // 
+            // saladButton
+            // 
+            this.saladButton.Location = new System.Drawing.Point(138, 274);
+            this.saladButton.Name = "saladButton";
+            this.saladButton.Size = new System.Drawing.Size(64, 43);
+            this.saladButton.TabIndex = 23;
+            this.saladButton.Text = "Caesar Salad";
+            this.saladButton.UseVisualStyleBackColor = true;
+            this.saladButton.Click += new System.EventHandler(this.saladButton_Click);
+            // 
+            // receipttitleLabel
+            // 
+            this.receipttitleLabel.Font = new System.Drawing.Font("MS Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receipttitleLabel.Location = new System.Drawing.Point(346, 51);
+            this.receipttitleLabel.Name = "receipttitleLabel";
+            this.receipttitleLabel.Size = new System.Drawing.Size(165, 44);
+            this.receipttitleLabel.TabIndex = 26;
+            this.receipttitleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.Location = new System.Drawing.Point(256, 345);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(39, 20);
+            this.tenderedInput.TabIndex = 27;
+            this.tenderedInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(186, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 23);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Tendered:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pricingLabel
+            // 
+            this.pricingLabel.Font = new System.Drawing.Font("MS UI Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pricingLabel.Location = new System.Drawing.Point(466, 95);
+            this.pricingLabel.Name = "pricingLabel";
+            this.pricingLabel.Size = new System.Drawing.Size(45, 273);
+            this.pricingLabel.TabIndex = 29;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CashRegister.Properties.Resources._213;
+            this.ClientSize = new System.Drawing.Size(527, 409);
+            this.Controls.Add(this.pricingLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tenderedInput);
+            this.Controls.Add(this.receipttitleLabel);
+            this.Controls.Add(this.wingsButton);
+            this.Controls.Add(this.garlicbreadButton);
+            this.Controls.Add(this.saladButton);
+            this.Controls.Add(this.extrasLabel);
+            this.Controls.Add(this.onionLabel);
+            this.Controls.Add(this.greenpepperButton);
+            this.Controls.Add(this.neworderButton);
+            this.Controls.Add(this.receiptLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ordertitleLabel);
+            this.Controls.Add(this.orderLabel);
+            this.Controls.Add(this.mushroomButton);
+            this.Controls.Add(this.pineappleButton);
+            this.Controls.Add(this.extrachzButton);
+            this.Controls.Add(this.pepperoniButton);
+            this.Controls.Add(this.toppingsLabel);
+            this.Controls.Add(this.lrgButton);
+            this.Controls.Add(this.medButton);
+            this.Controls.Add(this.smallButton);
+            this.Controls.Add(this.sizeLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CashRegister";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label sizeLabel;
+        private System.Windows.Forms.Button smallButton;
+        private System.Windows.Forms.Button medButton;
+        private System.Windows.Forms.Button lrgButton;
+        private System.Windows.Forms.Label toppingsLabel;
+        private System.Windows.Forms.Button pepperoniButton;
+        private System.Windows.Forms.Button extrachzButton;
+        private System.Windows.Forms.Button pineappleButton;
+        private System.Windows.Forms.Button mushroomButton;
+        private System.Windows.Forms.Label orderLabel;
+        private System.Windows.Forms.Label ordertitleLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button neworderButton;
+        private System.Windows.Forms.Label receiptLabel;
+        private System.Windows.Forms.Button onionLabel;
+        private System.Windows.Forms.Button greenpepperButton;
+        private System.Windows.Forms.Label extrasLabel;
+        private System.Windows.Forms.Button wingsButton;
+        private System.Windows.Forms.Button garlicbreadButton;
+        private System.Windows.Forms.Button saladButton;
+        private System.Windows.Forms.Label receipttitleLabel;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label pricingLabel;
+    }
+}
+
---
+++ CashRegister/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CashRegister
+{
+    public partial class Form1 : Form
+    {
+        string size;
+        string toppings;
+        string extras;
+        double subtotal;
+        double total;
+        double tax = 1.13;
+        int pepperoni = 0;
+        int extracheese = 0;
+        int greenpeppers = 0;
+        int pineapple = 0;
+        int mushroom = 0;
+        int onions = 0;
+        double smallPrice = 10.25;
+        double mediumPrice = 14.25;
+        double largePrice = 18.25;
+        double toppingPrice = 1.25;
+        double garlicBreadPrice = 8.99;
+        double saladPrice = 6.99;
+        double chickenPrice = 10.99;
+        double garlicBread = 0;
+        double salad = 0;
+        double chicken = 0;
+        double tendered;
+        double change;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            receipttitleLabel.Visible = false;
+            receiptLabel.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lrgButton_Click(object sender, EventArgs e)
+        {
+            size = "LARGE";
+            orderLabel.Text += "\nLarge";
+            Convert.ToString(largePrice);
+            subtotal += 18.25;
+            medButton.Enabled = false;
+            smallButton.Enabled = false;
+
+        }
+
+        private void smallButton_Click(object sender, EventArgs e)
+        {
+            size = "SMALL";
+            Convert.ToString(smallPrice);
+            subtotal += 10.25;
+            medButton.Enabled = false;
+            lrgButton.Enabled = false;
+        }
+
+        private void medButton_Click(object sender, EventArgs e)
+        {
+            size = "MEDIUM";
+            Convert.ToString(mediumPrice);
+            subtotal += 14.25;
+            lrgButton.Enabled = false;
+            smallButton.Enabled = false;
+        }
+
+        private void pepperoniButton_Click(object sender, EventArgs e)
+        {
+            orderLabel.Text += "\nPepperoni";
+            pepperoni++;
+            subtotal+=1.25;
+            Convert.ToString(toppingPrice);
+
+        }
+
+        private void extrachzButton_Click(object sender, EventArgs e)
+        {
+            orderLabel.Text += "\nExtra Cheese";
+            extracheese++;
+            subtotal += 1.25;
+
+        }
+
+        private void greenpepperButton_Click(object sender, EventArgs e)
+        {
+            orderLabel.Text += "\nGreen Peppers";
+            greenpeppers++;
+            subtotal += 1.25;
+        }
+
+        private void pineappleButton_Click(object sender, EventArgs e)
+        {
+            orderLabel.Text += "\nPineapple";
+            pineapple++;
+            subtotal += 1.25;
+          //  receiptpriceLabel.Text += $"\n{toppingPrice}";
+        }
+
+        private void mushroomButton_Click(object sender, EventArgs e)
+        {
+            orderLabel.Text += "\nMushroom";
+            mushroom++;
+            subtotal += 1.25;
+           // receiptpriceLabel.Text += $"\n{toppingPrice}";
+        }
+
+        private void onionLabel_Click(object sender, EventArgs e)
+        {
+            orderLabel.Text += "\nOnions";
+            onions++;
+            subtotal += 1.25;
+           // receiptpriceLabel.Text += $"\n{toppingPrice}";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            receiptLabel.Visible = true;
+            receipttitleLabel.Visible = true;
+            receipttitleLabel.Text += $"\nRECEIPT\n";
+            Thread.Sleep(100);
+            Refresh();
+            receipttitleLabel.Text += $"PINOS PIZZA";
+            Thread.Sleep(100);
+            Refresh();
+            receiptLabel.Text += $"\n1 {size} PIZZA\n";
+            pricingLabel.Text += $"\n{mediumPrice}\n";
+            Thread.Sleep(100);
+            Refresh();
+            receiptLabel.Text += $"\nPeperoni x{pepperoni}";
+            pricingLabel.Text += $"\n{pepperoni*toppingPrice}";
+            Thread.Sleep(100);
+            Refresh();
+            receiptLabel.Text += $"\nExtra Cheese x{extracheese}";
+            pricingLabel.Text += $"\n{extracheese * toppingPrice}";
+            Thread.Sleep(100);
+            Refresh();
+            receiptLabel.Text += $"\nGreen Peppers x{greenpeppers}";
+            pricingLabel.Text += $"\n{greenpeppers * toppingPrice}";
+            Thread.Sleep(100);
+            Refresh();
+            receiptLabel.Text += $"\nPineapple x{pineapple}";
+            pricingLabel.Text += $"\n{pineapple * toppingPrice}";
+            Thread.Sleep(100);
+            Refresh();
+            receiptLabel.Text += $"\nMushrooms x{mushroom}";
+            pricingLabel.Text += $"\n{mushroom * toppingPrice}";
+            Thread.Sleep(100);
+            Refresh();
+            receiptLabel.Text += $"\nOnions x{onions}";
+            pricingLabel.Text += $"\n{onions * toppingPrice}";
+            Thread.Sleep(100);
+            Refresh();
+            receiptLabel.Text += $"\nCaesar Salad x{salad}";
+            pricingLabel.Text += $"\n{salad * toppingPrice}";
+            Thread.Sleep(100);
+            Refresh();
+            receiptLabel.Text += $"\nGarlic Bread x{garlicBread}";
+            pricingLabel.Text += $"\n{garlicBread * toppingPrice}";
+            Thread.Sleep(100);
+            Refresh();
+            receiptLabel.Text += $"\nChicken Wings x{chicken}";
+            pricingLabel.Text += $"\n{chicken * toppingPrice}";
+            Thread.Sleep(100);
+            Refresh();
+            receiptLabel.Text += $"\n\n\n\n\n\n\nSubtotal\nTax\nTotal\n\nTendered\nChange";
+            pricingLabel.Text += $"\n\n\n\n\n\n\n{subtotal}\n{tax*subtotal-subtotal}\n{tax*subtotal}\n\n{tendered}\n{tendered-total}";
+        }
+
+        private void saladButton_Click(object sender, EventArgs e)
+        {
+            orderLabel.Text += "\nCaesar Salad";
+            salad++;
+            subtotal += 6.99;
+          // receiptpriceLabel.Text += $"\n{saladPrice}";
+        }
+
+        private void garlicbreadButton_Click(object sender, EventArgs e)
+        {
+            orderLabel.Text += "\nGarlic Bread";
+            garlicBread++;
+            subtotal += 8.99;
+            //receiptpriceLabel.Text += $"\n{garlicBreadPrice}";
+        }
+
+        private void wingsButton_Click(object sender, EventArgs e)
+        {
+            orderLabel.Text += "\nChicken Wings";
+            chicken++;
+            subtotal += 10.99;
+            //receiptpriceLabel.Text += $"\n{garlicBreadPrice}";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           Convert.ToString(tenderedInput);
+         //   tendered = tenderedInput.Text;
+      
+        }
+    }
+}
